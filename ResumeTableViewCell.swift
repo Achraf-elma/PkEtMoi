734a1da67e412f0eb9563cdf9c32a3188da08b94
Kevin Giordani <kevingiordani@macbook-air-de-kevin.local>
2018-03-20 12:16:23 +0100

    TAMERE

---
+++ ResumeTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  ResumeTableViewCell.swift
+//  PkEtMoi
+//
+//  Created by Kevin Giordani on 19/03/2018.
+//  Copyright © 2018 Kevin GIORDANI. All rights reserved.
+//
+
+import UIKit
+
+class ResumeTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var labelCell: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
